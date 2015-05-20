f893ab1caa72d2dd37cbae61a5db0c64955e0b0d
emsk <emsk1987@gmail.com>
2015-05-21 01:07:27 +0900

    Add documentation for the Git API

--- lib/backlog_kit/client/git.rb
+++ lib/backlog_kit/client/git.rb
@@ -1,6 +1,13 @@
 module BacklogKit
   class Client
+
+    # Methods for the Git API
     module Git
+
+      # Get list of git repositories
+      #
+      # @param project_id_or_key [Integer, String] Project id or project key
+      # @return [BacklogKit::Response] List of git repositories
       def get_git_repositories(project_id_or_key)
         get('git/repositories', project_id_or_key: project_id_or_key)
       end
