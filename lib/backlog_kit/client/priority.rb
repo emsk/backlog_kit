2d82fe75f9e93b43b1fe6a0ff5f88fcae91322fd
emsk <emsk1987@gmail.com>
2015-05-22 00:47:41 +0900

    Add documentation for the Priority API

--- lib/backlog_kit/client/priority.rb
+++ lib/backlog_kit/client/priority.rb
@@ -1,6 +1,12 @@
 module BacklogKit
   class Client
+
+    # Methods for the Priority API
     module Priority
+
+      # Get list of priorities
+      #
+      # @return [BacklogKit::Response] List of priorities
       def get_priorities
         get('priorities')
       end
