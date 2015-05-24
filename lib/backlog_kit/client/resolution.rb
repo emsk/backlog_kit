b81d9259cc8e4d5b7f28c08953872c1c55662856
emsk <emsk1987@gmail.com>
2015-05-24 14:34:28 +0900

    Add documentation for the Resolution API

--- lib/backlog_kit/client/resolution.rb
+++ lib/backlog_kit/client/resolution.rb
@@ -1,6 +1,12 @@
 module BacklogKit
   class Client
+
+    # Methods for the Resolution API
     module Resolution
+
+      # Get list of resolutions
+      #
+      # @return [BacklogKit::Response] List of resolutions
       def get_resolutions
         get('resolutions')
       end
