d77f4e3cc8e0f9fe181f6da1e6f7d821645c47be
emsk <emsk1987@gmail.com>
2015-05-25 00:37:16 +0900

    Add documentation for the Status API

--- lib/backlog_kit/client/status.rb
+++ lib/backlog_kit/client/status.rb
@@ -1,6 +1,12 @@
 module BacklogKit
   class Client
+
+    # Methods for the Status API
     module Status
+
+      # Get list of statuses
+      #
+      # @return [BacklogKit::Response] List of statuses
       def get_statuses
         get('statuses')
       end
