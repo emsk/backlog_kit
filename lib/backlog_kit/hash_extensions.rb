533d71bb43235d9984b9a6ebc685c1ec0584a605
emsk <emsk1987@gmail.com>
2015-06-21 22:15:52 +0900

    Add documentation for Hash#camelize_keys!

--- lib/backlog_kit/hash_extensions.rb
+++ lib/backlog_kit/hash_extensions.rb
@@ -1,6 +1,10 @@
 require 'active_support/core_ext/hash/keys'
 
 class Hash
+
+  # Camelize all keys of the hash
+  #
+  # @return [Hash] The hash with camelized keys
   def camelize_keys!
     deep_transform_keys! do |key|
       key.to_s
